436a195c7db046c530a11a721517b61c5bc1b623
Katharina Wiedmann <katie.wiedmann@gmx.de>
2018-11-20 10:45:31 +0000

    Shortened first function which listens to the button clicking and changed last counting if statement to a switch statement

--- js/engine.js
+++ js/engine.js
@@ -6,42 +6,26 @@ var lost = 0;
 var draw = 0;
 
 
-/*listen for the button clicks */
-$("#rock").click(function(event){
-  var play_you = "rock";
-$("#play_you").text(play_you);/*shows what you choose*/
-var play_machine = computer_move(); /*look at function below */
-compare (play_you, play_machine);
-});
-
-$("#paper").click(function(event){
-  var play_you = "paper";
-$("#play_you").text(play_you); /*shows what you choose*/
-var play_machine = computer_move();
-compare (play_you, play_machine);
-});
-
-$("#scissors").click(function(event){
-  var play_you = "scissors";
-$("#play_you").text(play_you); /*shows what you choose*/
-var play_machine = computer_move();
-compare (play_you, play_machine);
+/*listen for the button clicks, doesn't matter which button */
+$("button").click(function(event) {
+  var play_you = this.getAttribute("value"); /* bc of "this" computer knows which button has been clicked, could be rock, paper or scissors*/
+  $("#play_you").text(play_you); /*shows what you choose*/
+  var play_machine = computer_move(); /*assigns play_machine to whatever the computer did - look at function below */
+  compare(play_you, play_machine);
 });
 
 function computer_move() {
-  var play_machine = Math.floor(Math.random()*(3-0)) + 0; /*computer choses random no. between 0-2*/
-play_machine = choices[play_machine]; /*no. from play_machine is assigned to the choices list, e.g. 1=paper*/
-$("#play_machine").text(play_machine); /*shows what the machine chooses*/
-
-
-return play_machine; /*show what the machine has played? WHY AND WHERE????*/
+  var play_machine = Math.floor(Math.random() * (3 - 0)) + 0; /*computer choses random no. between 0-2*/
+  play_machine = choices[play_machine]; /*no. from play_machine is assigned to the choices list, e.g. 1=paper*/
+  $("#play_machine").text(play_machine); /*shows what the machine chooses*/
+  return play_machine; /*show what the machine has played behind play_machine which is behind Machine:, see HTML*/
 }
 
-function compare(you, machine){
+function compare(you, machine) {
   var verdict = 'lose'; /* you lose unless you draw or win*/
 
-  if( you == machine ) {
-  verdict ='draw';
+  if (you == machine) {
+    verdict = 'draw';
   }
   if ((you == "rock") && (machine == "scissors")) {
     verdict = 'win';
@@ -52,21 +36,27 @@ function compare(you, machine){
   if ((you == "scissors") && (machine == "paper")) {
     verdict = 'win';
   }
-if (verdict == 'win') {
-  won++; /*if you win once more add a win*/
-  $("#game_won").text(won); /*if you win, show this*/
-}
-if (verdict == 'lose') {
-  lost++;/*if you lose once more add a lose*/
-  $("#game_lost").text(lost);/*if you lose, show this*/
-}
-if (verdict == 'draw') {
-  draw++; /*if you draw once more add a draw*/
-  $("#game_draw").text(draw);/*if you draw, show this*/
-}
 
-$("#verdict").text(verdict);
+  switch (verdict) { /*switch statement to make the code shorter*/
+    case 'win':
+      /*if it is a win, like defined above do the following*/
+      won++; /*if you win once more add a win*/
+      $("#game_won").text(won); /*if you win, show this*/
+      break;
 
-played++; /*add one to this variable*/
-$("#game_played").text(played);
+    case 'lose':
+      lost++;
+      $("#game_lost").text(lost);
+      break;
+
+    case 'draw':
+      draw++;
+      $("#game_draw").text(draw);
+      break;
+  }
+
+  $("#verdict").text(verdict);
+
+  played++; /*add one to this variable*/
+  $("#game_played").text(played);
 }
